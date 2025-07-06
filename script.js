d6a23e5ab6e6ec7d42d4e2b07f2cff851b89f656
bdandina <bayuandien@outlook.com>
2025-07-06 20:40:41 +0700

    update dark mode transisi

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
-// Cek status dark mode saat halaman dimuat
-window.onload = function () {
-  const darkMode = localStorage.getItem('darkMode');
-  if (darkMode === 'enabled') {
-    document.body.classList.add('dark');
-  }
-};
-
-// Fungsi untuk mengaktifkan/mematikan dark mode
 function toggleDarkMode() {
-  document.body.classList.toggle('dark');
+  document.documentElement.classList.toggle('dark');
 
-  if (document.body.classList.contains('dark')) {
+  if (document.documentElement.classList.contains('dark')) {
     localStorage.setItem('darkMode', 'enabled');
   } else {
     localStorage.setItem('darkMode', 'disabled');
